512196b89aa1c0f8b0553f9ad0e21a445ede7f28
Huihyeon Park <agnea0829@gmail.com>
2024-06-28 11:06:44 +0900

    #4 21-3. 클래스 생성

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace CSPractice1
 {
+    class FirstClass
+    {
+
+    }
     internal class Program
     {
+        class ThirdClass
+        {
+
+        }
         static void Main(string[] args)
         {
             //#1 12-1. 기본용어
@@ -157,6 +165,8 @@ namespace CSPractice1
             Console.WriteLine(Math.Min(42, 1)); //1
             Console.WriteLine(Math.Max(42, 1)); //42
             Console.WriteLine(Math.PI); //3.141592
+
+            // #4 21-3. 클래스 생성
 
 
         }
